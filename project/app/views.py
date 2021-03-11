c79edbd71c97d11fb45638051f790c235ce6566b
Pachocinski <pachocinski@MacBook-Pro-de-Pachocinski.local>
2021-03-11 12:07:07 +0100

    add mail in contact form

--- project/app/views.py
+++ project/app/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponseRedirect
 from .models import Fruits, Contact
 from app.forms import ContactForm
 import datetime
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
+
 # Create your views here.
 def index(request):
     test = 'wesh'
@@ -22,8 +26,17 @@ def contact(request):
                 message = form.cleaned_data['message'],
                 # created_at = date.today(),
                 # updated_at = datetime.datetime.now()
-                
-           )    
+            )
+            msg_plain = ''
+            msg_html = render_to_string('app/emails/welcome.html', {'name': form.cleaned_data['name']})
+            objet = "Message bien recu " + form.cleaned_data['name'] + " !"
+            send_mail(
+                objet,
+                msg_plain,
+                settings.EMAIL_HOST_USER,
+                [form.cleaned_data['mail']],
+                html_message=msg_html,
+            )
             form = ContactForm()
     return render(request, 'app/contact.html', {'form': form})
 
